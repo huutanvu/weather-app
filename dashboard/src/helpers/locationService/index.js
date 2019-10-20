6653554e21601dd81a03b0084f781ce446ada3e7
Tan Vu <tanvu.nus@gmail.com>
2019-10-20 11:58:46 -0400

    Added the open_cage_data_api to forward and reverse geocoding

--- dashboard/src/helpers/locationService/index.js
+++ dashboard/src/helpers/locationService/index.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 // Does not support http calls
 // const IP_API_URL = "http://ip-api.com/json/"; 
 
+// OpenCageData api: https://opencagedata.com/demo
+const OPEN_CAGE_DATA_API = "https://api.opencagedata.com/geocode/v1/json"
+const OPEN_CAGE_API_KEY = process.env.REACT_APP_OPEN_CAGE_API_KEY;
+
 class LocationService {
 
     getCurrentPosition = () => {
@@ -10,6 +14,53 @@ class LocationService {
             return new Promise((resolve, reject) => {
                 navigator.geolocation.getCurrentPosition(resolve, reject)
             })
+        }
+    }
+
+    getReverseLocation = async (location) => {
+        const locationString = `${location.lat},${location.lon}`;
+        console.log(locationString)
+        try {
+            let response = await axios.get(OPEN_CAGE_DATA_API, {
+                params: {
+                    q: locationString,
+                    key: OPEN_CAGE_API_KEY
+                }
+            });
+            let allInfos = response.data;
+            if (allInfos.total_results > 0) {
+                let city = allInfos.results[0].components.city;
+                let country = allInfos.results[0].components.country;
+                return { city, country };
+            }
+            else {
+                return null;
+            }
+        }
+        catch (err) {
+            console.error(err);
+        }
+    }
+
+    getLocation = async (cityName, country) => {
+        const locationString = `${cityName},${country}`;
+        try {
+            let response = await axios.get(OPEN_CAGE_DATA_API, {
+                params: {
+                    q: locationString,
+                    key: OPEN_CAGE_API_KEY
+                }
+            });
+            let allInfos = response.data;
+            let location = {};
+            if (allInfos.total_results > 0) {
+                location["lon"] = allInfos.results[0].geometry.lng;
+                location["lat"] = allInfos.results[0].geometry.lat;
+            }
+            return location;
+        }
+        catch (err) {
+            console.error(err);
         }
     }
 
